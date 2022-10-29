0883d23923461d433ba4f445b3cb60bcca4cfbc0
HaoxiangKT1 <116784968+HaoxiangKT1@users.noreply.github.com>
2022-10-29 21:20:23 +0800

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,35 @@
+require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+const fs = require('fs');
+// const infuraId = fs.readFileSync(".infuraid").toString().trim() || "";
+require('dotenv').config();
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    goerli: {
+      url: https://eth-goerli.g.alchemy.com/v2/gEwN6RRwCorpwAcuqAOyD-L8UOk0Zxeg,
+      accounts: [ 02a3f4a3fec54da50a0219ed29664dde444632cb36fd268df8307b51a9f3448d ]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
